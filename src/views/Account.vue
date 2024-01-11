9aa38e4cc7ef9ee5077db04bb4e835266bd1eea3
AbyRin <cpr020620@outlook.com>
2024-01-11 23:07:31 +0800

    daily update

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -2,15 +2,40 @@
   <div>
     <!-- 账户信息 -->
     <div class="account_div">
-      <div>
+      <div class="avatar_div">
         <img :src="userData.avatar" alt="">
-        <!-- 注：user表未设置头像字段 -->
       </div>
-      <div>
-        <p>{{ userData.nickName }}</p>
-        <p>{{ userData.email }}</p>
 
+      <div class="account_info_div">
+        <p id="nickname_p">
+          {{ userData.nickName }}
+        </p>
+        <p id="email_p">
+          {{ userData.email }}
+        </p>
       </div>
+    </div>
+
+    <!-- 折叠面板-地址管理  -->
+    <div class="address_collapse">
+      <div class="collapse_title">我的地址</div>
+      <el-collapse v-model="activeNames" class="collapse_block" @change="handleChange">
+        <el-collapse-item title="地址1" name="1">
+          <p>测试-收货人姓名</p>
+          <p>测试-收货人电话</p>
+          <p>测试-收货人地址</p>
+        </el-collapse-item>
+        <el-collapse-item title="地址2" name="2">
+          <p>测试-收货人姓名</p>
+          <p>测试-收货人电话</p>
+          <p>测试-收货人地址</p>
+        </el-collapse-item>
+        <el-collapse-item title="地址3" name="3">
+          <p>测试-收货人姓名</p>
+          <p>测试-收货人电话</p>
+          <p>测试-收货人地址</p>
+        </el-collapse-item>
+      </el-collapse>
     </div>
   </div>
 </template>
@@ -20,8 +45,15 @@ export default {
     data() {
         return {
             // 本地存储的用户信息（检查是否为空，不为空则解析为 JSON 对象）
-            userData: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+            userData: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {},
+            // 折叠面板
+            activeNames: ['1'],
         };
+    },
+    methods: {
+        handleChange(val) {
+            console.log(val);
+        }
     },
 }
 </script>
@@ -37,12 +69,12 @@ body{
     width: 100%;
     min-width: 1200px;
 }
-$account_div_width: 800px;  // 账户信息-总宽度
+$account_div_width: 640px;  // 账户信息-总宽度
 /* 账户信息 */
 .account_div {
     position: fixed;
     width: $account_div_width;
-    height: 800px;
+    height: 760px;
 
     left: 60px;
     bottom: 0;
@@ -51,5 +83,61 @@ $account_div_width: 800px;  // 账户信息-总宽度
     box-shadow: 5px 5px 10px rgba(0,0,0,0.4);
     border-radius: 20px 20px 0 0;
     text-align: center;
+
+    .avatar_div {
+        margin-top: 40px;
+        img {
+            width: 150px;
+            height: 150px;
+            border-radius: 50%;
+        }
+    }
+
+    .account_info_div {
+        p {
+            &[id="nickname_p"] {
+                margin-top: 20px;
+                color: $blue_color;
+                font: {
+                    size: 24px;
+                    weight: bold;
+                }
+            }
+            &[id="email_p"] {
+                margin-top: 20px;
+                color: #888888;
+            }
+
+        }
+    }
+}
+
+
+// 折叠面板-地址管理
+.address_collapse {
+    position: fixed;
+    width: 600px;
+    height: auto;
+
+    right: 240px;
+    top: 500px;
+
+    background-color: #ffffff;
+    box-shadow: 5px 5px 10px rgba(0,0,0,0.4);
+    border-radius: 20px 20px 0 0;
+
+    .collapse_title {
+        text-align: center;
+        font: {
+            size: 24px;
+            weight: bold;
+        };
+        color: $blue_color;
+    }
+
+    .collapse_block {
+        border-radius: 20px 20px 0 0;
+        background-color: red;
+    }
 }
 </style>
